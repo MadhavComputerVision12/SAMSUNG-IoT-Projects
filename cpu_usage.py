25598e3666d4ea8e6526c101d6e4e4e300ab5491
Madhav Sharma <sharmamadhav1204@gmail.com>
2024-11-19 11:49:08 +0530

    Delete cpu_usage.py

--- cpu_usage.py
+++
@@ -1,9 +0,0 @@
-from gpiozero import LED
-import psutil
-from time import sleep
-from datetime import datetime
-
-led_yellow=LED(20)
-led_red=LED(21)
-file=open("/home/madhav12/Desktop/cpu_usage_log.txt","w")
-
